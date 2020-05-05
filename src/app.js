793e731f4c9e8ec7883c77172ec6fe2f3f9b4d93
Fiorela <fbasiliomanrique@gmail.com>
2020-05-04 23:16:03 -0500

    Haciendo cambios en el responsive css

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $("#baner").css({"height":$(window).height() + "px"});
+// });
+
+// $(window).scroll(function() {
+//     if ($("#menu").offset().top > 56) {
+//         $("#menu").addClass("bg-dark");
+//     } else {
+//         $("#menu").removeClass("bg-dark");
+//     }
+//   });
